2cfa72ae96c22260185ceb718cc156d710e43801
CrackerJaps <seantokuzo@gmail.com>
2022-02-14 15:21:27 -0800

    setup audio folder and sounds object

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
-import React from "react"
+import React, { useState } from "react"
 
 function App() {
-  
 
   return (
       <div id="drum-machine">
---
+++ src/audio/sounds.js
@@ -0,0 +1,55 @@
+const sounds = {
+    claps: [
+        'soft-clap',
+        'tape-clap',
+        'trap-clap'
+    ],
+    fun: [
+        'deadly-narcotic',
+        'look-at-his-face',
+        'okay',
+        'what-did-it-say',
+        'where-gun'
+    ],
+    hats: [
+        'hard-hh',
+        'live-hh',
+        'lofi-hh',
+        'low-hh',
+        'lowkey-hh-loop',
+        'upbeat-hh-loop'
+    ],
+    kicks: [
+        'live-kick',
+        'lofi-kick',
+        'sub-kick'
+    ],
+    percs: [
+        'camera-perc',
+        'hand-drum-perc'
+    ],
+    toms: [
+        'floor-tom',
+        'rack-tom'
+    ],
+    tracks: [
+        'dreamy-guitar',
+        'funky-guitar',
+        'lofi-guitar',
+        'piano-vibes',
+        'vintage-piano'
+    ],
+    vfx: [
+        'vfx-1',
+        'vfx-2',
+        'vfx-3',
+        'vfx-4',
+        'vfx-5',
+        'vfx-6',
+        'vfx-7',
+        'vfx-8',
+        'vfx-9',
+    ]
+}
+
+export default sounds
